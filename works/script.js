ea0ac01107fd6a3aadb56a997d1cf711d28341ce
Jenny Zhu <jz2915@nyu.edu>
2019-05-19 00:29:53 +0800

    05/18/2019

--- works/script.js
+++ works/script.js
@@ -3,22 +3,26 @@ var comic = document.getElementById("comic");
 var flyby = document.getElementById("flyby");
 var gravity = document.getElementById("gravity");
 var agent = document.getElementById("agent");
+var flow = document.getElementById("flow");
 var novelText = document.getElementById("novelText");
 var comicText = document.getElementById("comicText");
 var flybyText = document.getElementById("flybyText");
 var gravityText = document.getElementById("gravityText");
 var agentText = document.getElementById("agentText");
+var flowText = document.getElementById("flowText");
 
 novel.addEventListener("mouseover",function(){show(novelText)});
 comic.addEventListener("mouseover",function(){show(comicText)});
 flyby.addEventListener("mouseover",function(){show(flybyText)});
 gravity.addEventListener("mouseover",function(){show(gravityText)});
 agent.addEventListener("mouseover",function(){show(agentText)});
+flow.addEventListener("mouseover",function(){show(flowText)});
 novelText.addEventListener("mouseout",function(){hide(this)});
 comicText.addEventListener("mouseout",function(){hide(this)});
 flybyText.addEventListener("mouseout",function(){hide(this)});
 gravityText.addEventListener("mouseout",function(){hide(this)});
 agentText.addEventListener("mouseout",function(){hide(this)});
+flowText.addEventListener("mouseout",function(){hide(this)});
 
 function show(e){
   e.style.visibility = "visible";
